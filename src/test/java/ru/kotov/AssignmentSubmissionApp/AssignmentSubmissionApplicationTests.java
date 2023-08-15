a4d467bb135ade60617ccfcf22c10bd9273a99b1
DimaKotovProgrammist <dskotov003@gmail.com>
2023-08-15 12:03:01 +0300

    initial commit with db configuration

---
+++ src/test/java/ru/kotov/AssignmentSubmissionApp/AssignmentSubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kotov.AssignmentSubmissionApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentSubmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
