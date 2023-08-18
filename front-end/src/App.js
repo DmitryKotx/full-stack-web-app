c513e2c827d8cb1076f39ddcfb46ec9adebacbfe
DimaKotovProgrammist <dskotov003@gmail.com>
2023-08-18 17:53:29 +0300

    added extraction of the JWT token and adding it to local storage

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import { useLocalState } from './util/useLocalStorage';
 
 function App() {
+
+  const [jwt, setJwt] = useLocalState("", "jwt");
+
+  useEffect(() => {
+    if (!jwt) {
+      const reqBody = {
+        username : "aboba",
+        password : "1234"
+      }
+      fetch("api/v1/auth/register", {
+        headers: {
+          'Access-Control-Allow-Origin': 'http://localhost:3000', // Указать ваш фронтенд
+          'Content-Type': 'application/json',
+        },
+        method : "post",
+        body : JSON.stringify(reqBody)
+      })
+      .then(response => response.json())
+      .then(data => {
+        setJwt(data["token"]);
+      })
+    }
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world</h1>
+      <div>JWT Value is {jwt}</div>
     </div>
   );
 }
