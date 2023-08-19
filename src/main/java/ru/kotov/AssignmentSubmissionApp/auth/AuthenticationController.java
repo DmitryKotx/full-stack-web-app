ca0c82795e3a5e8c61aec4e004ff66801048c645
DimaKotovProgrammist <dskotov003@gmail.com>
2023-08-20 00:31:30 +0300

    the token is now passed in headers instead of the body

--- src/main/java/ru/kotov/AssignmentSubmissionApp/auth/AuthenticationController.java
+++ src/main/java/ru/kotov/AssignmentSubmissionApp/auth/AuthenticationController.java
@@ -1,6 +1,7 @@
 package ru.kotov.AssignmentSubmissionApp.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,18 +15,17 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(
+    public ResponseEntity<?> register(
             @RequestBody RegisterRequest request
     ) {
-        return ResponseEntity.ok(authenticationService.register(request));
+        return ResponseEntity.ok().header(HttpHeaders.AUTHORIZATION, String.valueOf(authenticationService.register(request))).build();
     }
 
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> authenticate(
+    public ResponseEntity<?> authenticate(
             @RequestBody AuthenticationRequest request
     ) {
-        return ResponseEntity.ok(authenticationService.authenticate(request));
-
+        return ResponseEntity.ok().header(HttpHeaders.AUTHORIZATION, String.valueOf(authenticationService.authenticate(request))).build();
     }
 
 }
