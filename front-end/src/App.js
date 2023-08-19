4bac97a79a4265c8f2d50fbd24d21bbb3cabacb3
DimaKotovProgrammist <dskotov003@gmail.com>
2023-08-20 00:36:52 +0300

    added connection of the ReactJS login page to Spring RESTful

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import './App.css';
 import Dashboard from './Dashboard';
 import HomePage from './HomePage';
@@ -10,27 +9,6 @@ import Login from './Login';
 function App() {
 
   const [jwt, setJwt] = useLocalState("", "jwt");
-
-  // useEffect(() => {
-  //   if (!jwt) {
-  //     const reqBody = {
-  //       username : "aboba",
-  //       password : "1234"
-  //     }
-  //     fetch("api/v1/auth/register", {
-  //       headers: {
-  //         'Access-Control-Allow-Origin': 'http://localhost:3000', // Указать ваш фронтенд
-  //         'Content-Type': 'application/json',
-  //       },
-  //       method : "post",
-  //       body : JSON.stringify(reqBody)
-  //     })
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       setJwt(data["token"]);
-  //     })
-  //   }
-  // }, [])
 
   return (
     <Routes>
--- front-end/src/Login/index.js
+++ front-end/src/Login/index.js
@@ -1,15 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLocalState } from '../util/useLocalStorage';
+import { func } from 'prop-types';
 
 const Login = () => {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    const [jwt, setJwt] = useLocalState("", "jwt");
+
+
+    console.log("username is: " + username);
+    console.log("password is: " + password);
+
+    function sendLoginRequest() {
+        const reqBody = {
+            username : username,
+            password : password
+        }
+        fetch("api/v1/auth/authenticate", {
+            headers: {
+            'Access-Control-Allow-Origin': 'http://localhost:3000',
+            'Content-Type': 'application/json',
+            },
+            method : "post",
+            body : JSON.stringify(reqBody)
+        })
+        .then(response => {
+            if(response.status === 200)
+                return Promise.all([response.json, response.headers]);
+            else return Promise.reject("Invalid login attempt");
+        })
+        .then(([body, headers]) => {
+            setJwt(headers.get("authorization"));
+            window.location.href = "dashboard";
+
+        })
+        .catch((message) => {
+            alert(message);
+        });
+    }
+
+  
+
     return (
         <>
             <div>
                 <label htmlFor='username'>Username</label>
-                <input type='email' id='username'/>
+                <input type='email' id='username' value={username}
+                 onChange={(event) => setUsername(event.target.value)}/>
             </div>
             <div>
             <label htmlFor='password'>Password</label>
-                <input type='password' id='password'/>
+                <input type='password' id='password'  value={password}
+                 onChange={(event) => setPassword(event.target.value)}/>
+            </div>
+            <div>
+                <button id='submit' type='button' onClick={() => sendLoginRequest()}>
+                    Login
+                </button>
             </div>
         </>
     );
