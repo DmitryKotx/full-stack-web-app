fd5ea68879487f5dd248667b10308fed30ece69a
DimaKotovProgrammist <dskotov003@gmail.com>
2023-08-19 22:06:13 +0300

    added the creation of a private React route that redirects if you are not logged in

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,37 +1,47 @@
 import { useEffect } from 'react';
 import './App.css';
+import Dashboard from './Dashboard';
+import HomePage from './HomePage';
+import { Route, Routes } from 'react-router-dom';
 import { useLocalState } from './util/useLocalStorage';
+import PrivateRout from './PrivateRoute';
+import Login from './Login';
 
 function App() {
 
   const [jwt, setJwt] = useLocalState("", "jwt");
 
-  useEffect(() => {
-    if (!jwt) {
-      const reqBody = {
-        username : "aboba",
-        password : "1234"
-      }
-      fetch("api/v1/auth/register", {
-        headers: {
-          'Access-Control-Allow-Origin': 'http://localhost:3000', // Указать ваш фронтенд
-          'Content-Type': 'application/json',
-        },
-        method : "post",
-        body : JSON.stringify(reqBody)
-      })
-      .then(response => response.json())
-      .then(data => {
-        setJwt(data["token"]);
-      })
-    }
-  }, [])
+  // useEffect(() => {
+  //   if (!jwt) {
+  //     const reqBody = {
+  //       username : "aboba",
+  //       password : "1234"
+  //     }
+  //     fetch("api/v1/auth/register", {
+  //       headers: {
+  //         'Access-Control-Allow-Origin': 'http://localhost:3000', // Указать ваш фронтенд
+  //         'Content-Type': 'application/json',
+  //       },
+  //       method : "post",
+  //       body : JSON.stringify(reqBody)
+  //     })
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       setJwt(data["token"]);
+  //     })
+  //   }
+  // }, [])
 
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-      <div>JWT Value is {jwt}</div>
-    </div>
+    <Routes>
+      <Route path='/dashboard' element={
+        <PrivateRout>
+          <Dashboard />
+        </PrivateRout>
+      }/>
+      <Route path='/login' element={<Login />}/>
+      <Route path='/' element={<HomePage />}/>
+    </Routes>
   );
 }
 
