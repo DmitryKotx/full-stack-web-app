aeb0c11df3f8f49afdfdb901f1cb70058d2e349b
DimaKotovProgrammist <dskotov003@gmail.com>
2023-08-30 02:16:44 +0300

    added a dashboard view for code reviewers

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,21 +1,41 @@
 import "./App.css";
 import Dashboard from "./Dashboard";
 import HomePage from "./HomePage";
+import jwt_decode from "jwt-decode";
 import { Route, Routes } from "react-router-dom";
 import PrivateRout from "./PrivateRoute";
 import Login from "./Login";
 import AssignmentView from "./AssignmentView";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useLocalState } from "./util/useLocalStorage";
+import { useState } from "react";
+import CodeReviewerDashboard from "./CodeReviewerDashboard";
 
 function App() {
+    const [jwt, setJwt] = useLocalState("", "jwt");
+    const [roles, setRoles] = useState(getRolesFromJwt());
+
+    function getRolesFromJwt() {
+        if (jwt) {
+            const decodeJwt = jwt_decode(jwt);
+            return decodeJwt.authorities;
+        }
+    }
+
     return (
         <Routes>
             <Route
                 path="/dashboard"
                 element={
-                    <PrivateRout>
-                        <Dashboard />
-                    </PrivateRout>
+                    roles.find((role) => role === "REVIEWER") ? (
+                        <PrivateRout>
+                            <CodeReviewerDashboard />
+                        </PrivateRout>
+                    ) : (
+                        <PrivateRout>
+                            <Dashboard />
+                        </PrivateRout>
+                    )
                 }
             />
             <Route
