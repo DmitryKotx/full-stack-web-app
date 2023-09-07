752f5ed23d66258012e4ba64fe4288761825c777
DimaKotovProgrammist <dskotov003@gmail.com>
2023-09-07 11:40:04 +0300

    changed the name so that you can extract the id using useParams()

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -45,7 +45,7 @@ function App() {
             />
 
             <Route
-                path="/assignments/:id"
+                path="/assignments/:assignmentId"
                 element={
                     roles && roles.find((role) => role === "REVIEWER") ? (
                         <PrivateRout>
