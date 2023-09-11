d428dede72c902d7fb9c85416d4c365a6ecefaf9
DimaKotovProgrammist <dskotov003@gmail.com>
2023-09-11 23:56:15 +0300

    putting some methods and code into a separate component

---
+++ front-end/src/CommentContainer/index.js
@@ -0,0 +1,114 @@
+import React, { useEffect, useState } from "react";
+import { useUser } from "../UserProvider";
+import ajax from "../Services/fetchService";
+import { Button } from "bootstrap";
+import Comment from "../Comment";
+
+const CommentContainer = (props) => {
+    const { assignmentId } = props;
+    const user = useUser();
+    const emptyComment = {
+        id: null,
+        text: "",
+        assignmentId: assignmentId != null ? parseInt(assignmentId) : null,
+        user: user.jwt,
+    };
+    const [comment, setComment] = useState(emptyComment);
+    const [comments, setComments] = useState([]);
+
+    useEffect(() => {
+        ajax(
+            `/api/comments?assignmentId=${assignmentId}`,
+            "GET",
+            user.jwt,
+            null
+        ).then((commentsData) => {
+            setComments(commentsData);
+        });
+    }, []);
+
+    function updateComment(value) {
+        const commentCopy = { ...comment };
+        commentCopy.text = value;
+        setComment(commentCopy);
+    }
+
+    function submitComment() {
+        if (comment.id) {
+            ajax(`/api/comments/${comment.id}`, "PUT", user.jwt, comment).then(
+                (data) => {
+                    const commentsCopy = [...comments];
+                    const i = comments.findIndex(
+                        (comment) => comment.id === data.id
+                    );
+                    commentsCopy[i] = data;
+                    setComments(commentsCopy);
+                    setComment(emptyComment);
+                }
+            );
+        } else {
+            ajax("/api/comments", "POST", user.jwt, comment).then((data) => {
+                const commentsCopy = [...comments];
+                commentsCopy.push(data);
+                setComments(commentsCopy);
+                setComment(emptyComment);
+            });
+        }
+    }
+    function handleDeleteComment(commentId) {
+        ajax(`/api/comments/${commentId}`, "DELETE", user.jwt).then(() => {
+            const commentsCopy = [...comments];
+            const i = commentsCopy.findIndex(
+                (comment) => comment.id === commentId
+            );
+            console.log(commentsCopy);
+            commentsCopy.splice(i, 1);
+            console.log(commentsCopy);
+
+            setComments(commentsCopy);
+        });
+    }
+    function handleEditComment(commentId) {
+        const i = comments.findIndex((comment) => comment.id === commentId);
+        const commentCopy = {
+            id: comments[i].id,
+            text: comments[i].text,
+            assignmentId: assignmentId != null ? parseInt(assignmentId) : null,
+            user: user.jwt,
+        };
+        setComment(commentCopy);
+    }
+
+    function updateComment(value) {
+        const commentCopy = { ...comment };
+        commentCopy.text = value;
+        setComment(commentCopy);
+    }
+
+    return (
+        <>
+            <div className="mt-5">
+                <textarea
+                    style={{ width: "100%", borderRadius: "0.25em" }}
+                    onChange={(e) => updateComment(e.target.value)}
+                    value={comment.text}
+                ></textarea>
+                <Button onClick={() => submitComment()}>Post Comment</Button>
+            </div>
+            <div className="mt-5">
+                {comments.map((comment) => (
+                    <Comment
+                        createdDate={comment.createdDate}
+                        createdBy={comment.createdBy}
+                        text={comment.text}
+                        emitDeleteComment={handleDeleteComment}
+                        emitEditComment={handleEditComment}
+                        id={comment.id}
+                    />
+                ))}
+            </div>
+        </>
+    );
+};
+
+export default CommentContainer;
