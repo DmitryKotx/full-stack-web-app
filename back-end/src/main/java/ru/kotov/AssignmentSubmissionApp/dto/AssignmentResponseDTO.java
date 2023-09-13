896bd7353f7c2a5d73c27b3030bf62f60595bea6
DimaKotovProgrammist <dskotov003@gmail.com>
2023-09-13 23:52:14 +0300

    the entire server part has been moved to the back-end folder

--- back-end/src/main/java/ru/kotov/AssignmentSubmissionApp/dto/AssignmentResponseDTO.java
+++ back-end/src/main/java/ru/kotov/AssignmentSubmissionApp/dto/AssignmentResponseDTO.java
@@ -1,14 +1,12 @@
 package ru.kotov.AssignmentSubmissionApp.dto;
 
 import lombok.Data;
-import ru.kotov.AssignmentSubmissionApp.enums.AssignmentEnum;
 import ru.kotov.AssignmentSubmissionApp.enums.AssignmentStatusEnum;
 import ru.kotov.AssignmentSubmissionApp.model.Assignment;
 
 @Data
 public class AssignmentResponseDTO {
     private Assignment assignment;
-    private AssignmentEnum[] assignmentEnums = AssignmentEnum.values();
     private AssignmentStatusEnum[] statusEnums = AssignmentStatusEnum.values();
     public AssignmentResponseDTO(Assignment assignment) {
         super();
