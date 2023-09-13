e0e117cacc61403cc45b35607fb0492754862698
DimaKotovProgrammist <dskotov003@gmail.com>
2023-09-13 23:54:14 +0300

    adding Login and Register buttons for the home page

--- front-end/src/HomePage/index.js
+++ front-end/src/HomePage/index.js
@@ -1,9 +1,35 @@
 import React from "react";
+import { Button, Col, Row } from "react-bootstrap";
+import { useUser } from "../UserProvider";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+    const user = useUser();
+    const navigate = useNavigate();
+
     return (
-        <div>
+        <div className="center-container">
             <h1>HomePage</h1>
+            <div className="button-container mt-4">
+                <Button
+                    style={{ cursor: "pointer" }}
+                    onClick={() => {
+                        user.setJwt(null);
+                        navigate("/login");
+                    }}
+                >
+                    Login
+                </Button>
+                <Button
+                    style={{ cursor: "pointer" }}
+                    onClick={() => {
+                        user.setJwt(null);
+                        navigate("/register");
+                    }}
+                >
+                    Register
+                </Button>
+            </div>
         </div>
     );
 };
