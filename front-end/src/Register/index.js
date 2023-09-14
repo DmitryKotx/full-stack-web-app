bc6299198be074d16574f3237e28ee5f19f7d273
DimaKotovProgrammist <dskotov003@gmail.com>
2023-09-14 23:52:21 +0300

    added role selection on the registration page

--- front-end/src/Register/index.js
+++ front-end/src/Register/index.js
@@ -2,19 +2,30 @@ import React, { useEffect, useState } from "react";
 import { useUser } from "../UserProvider";
 import { useNavigate } from "react-router-dom";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import ajax from "../Services/fetchService";
 
 const Register = () => {
     const user = useUser();
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const [role, setRole] = useState(null);
+    const [roles, setRoles] = useState([]);
+    const [role, setRole] = useState("");
     const navigate = useNavigate();
+    const [selectedOption, setSelectedOption] = useState("");
 
     useEffect(() => {
         if (user.jwt) {
             navigate("/dashboard");
         }
+        ajax("/api/roles", "GET", user.jwt).then((roles) => {
+            setRoles(roles);
+        });
     }, [user.jwt]);
+
+    function handleOptionChange(event, role) {
+        setSelectedOption(event.target.value);
+        setRole(role);
+    }
 
     function sendLoginRequest() {
         const reqBody = {
@@ -30,6 +41,14 @@ const Register = () => {
             method: "POST",
             body: JSON.stringify(reqBody),
         })
+            .then((response) => {
+                if (response.status === 200)
+                    return Promise.all([response.json, response.headers]);
+                else
+                    return Promise.reject(
+                        "Incorrect data is entered or the role is not selected"
+                    );
+            })
             .then(([body, headers]) => {
                 user.setJwt(headers.get("authorization"));
                 navigate("/dashboard");
@@ -47,8 +66,8 @@ const Register = () => {
                         <Form.Group className="mb-3" controlId="username">
                             <label className="fs-4">Username</label>
                             <Form.Control
-                                type="email"
-                                id="username"
+                                type="username"
+                                controlId="username"
                                 size="lg"
                                 placeholder="Type in your email"
                                 value={username}
@@ -64,8 +83,8 @@ const Register = () => {
                         <Form.Group className="mb-3" controlId="password">
                             <label className="fs-4">Password</label>
                             <Form.Control
+                                controlId="password"
                                 type="password"
-                                id="password"
                                 size="lg"
                                 placeholder="Type in your password"
                                 value={password}
@@ -76,11 +95,43 @@ const Register = () => {
                         </Form.Group>
                     </Col>
                 </Row>
+
+                <Row className="justify-content-center">
+                    <Col
+                        md="12"
+                        lg="4"
+                        className="mt-3 d-flex flex-column gap-5 flex-md-row justify-content-md-between"
+                    >
+                        <div>
+                            <input
+                                type="radio"
+                                value="option1"
+                                checked={selectedOption === "option1"}
+                                onChange={(event) =>
+                                    handleOptionChange(event, roles[0])
+                                }
+                            />
+                            {roles[0]}
+                        </div>
+                        <h5>Choose a role</h5>
+                        <div>
+                            <input
+                                type="radio"
+                                value="option2"
+                                checked={selectedOption === "option2"}
+                                onChange={(event) =>
+                                    handleOptionChange(event, roles[1])
+                                }
+                            />
+                            {roles[1]}
+                        </div>
+                    </Col>
+                </Row>
                 <Row className="justify-content-center">
                     <Col
                         md="8"
                         lg="6"
-                        className="mt-2 d-flex flex-column gap-5 flex-md-row justify-content-md-between"
+                        className="mt-4 d-flex flex-column gap-5 flex-md-row justify-content-md-between"
                     >
                         <Button
                             id="submit"
@@ -88,7 +139,7 @@ const Register = () => {
                             onClick={() => sendLoginRequest()}
                         >
                             {" "}
-                            Login
+                            Register
                         </Button>
                         <Button
                             variant="secondary"
