fa2ab17a1e37b6843a4638a6ff93ccfa2141528a
DimaKotovProgrammist <dskotov003@gmail.com>
2023-09-20 23:35:34 +0300

    added the use of ObjectMapper for json generation

--- back-end/main/java/ru/kotov/AssignmentSubmissionApp/auth/AuthenticationController.java
+++ back-end/main/java/ru/kotov/AssignmentSubmissionApp/auth/AuthenticationController.java
@@ -1,7 +1,10 @@
 package ru.kotov.AssignmentSubmissionApp.auth;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
@@ -24,15 +27,18 @@ public class AuthenticationController {
     private final JwtUtil jwtUtil;
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody RegisterRequest request, BindingResult bindingResult) {
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request, BindingResult bindingResult) throws JsonProcessingException {
+        AuthenticationResponse response = authenticationService.register(request, bindingResult);
         if (bindingResult.hasErrors()) {
             Map<String, String> validationErrors = new HashMap<>();
             for (FieldError error : bindingResult.getFieldErrors()) {
                 validationErrors.put(error.getField(), error.getDefaultMessage());
             }
-            return ResponseEntity.badRequest().body(validationErrors);
+            ObjectMapper objectMapper = new ObjectMapper();
+            String json = objectMapper.writeValueAsString(validationErrors);
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(json);
         } else {
-            return ResponseEntity.ok().header(HttpHeaders.AUTHORIZATION, String.valueOf(authenticationService.register(request, bindingResult).getToken())).build();
+            return ResponseEntity.ok().header(HttpHeaders.AUTHORIZATION, String.valueOf(response.getToken())).body("{}");
         }
     }
 
