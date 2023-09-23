59c23f067b14be88c02a6a797df7e5148b8770c2
DimaKotovProgrammist <dskotov003@gmail.com>
2023-09-23 16:57:40 +0300

    added role checks and added exception handling

--- back-end/main/java/ru/kotov/AssignmentSubmissionApp/auth/AuthenticationService.java
+++ back-end/main/java/ru/kotov/AssignmentSubmissionApp/auth/AuthenticationService.java
@@ -6,34 +6,38 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import ru.kotov.AssignmentSubmissionApp.model.Authority;
 import ru.kotov.AssignmentSubmissionApp.model.User;
-import ru.kotov.AssignmentSubmissionApp.repository.UserRepository;
+import ru.kotov.AssignmentSubmissionApp.service.UserService;
 import ru.kotov.AssignmentSubmissionApp.util.JwtUtil;
+
 import java.time.LocalDate;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
-    private final UserRepository repository;
+    private final UserService userService;
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
     private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse register(RegisterRequest request, BindingResult bindingResult) {
-        if(!isValidPassword(request.getPassword())) {
-            bindingResult.rejectValue("password","",
-                    "The password does not meet the criteria:\n" +
-                    "1) at least one lowercase Latin letter\n" +
-                    "2) at least one capital Latin letter\n" +
-                    "3)at least one digit");
-            return new AuthenticationResponse();
+        if(request.getRole() == null)
+            bindingResult.rejectValue("role", "", "Role is not selected!");
+
+        if (!isValidPassword(request.getPassword())) {
+            bindingResult.rejectValue("password", "",
+                    """
+                            Password does not meet the criteria:
+                            1) at least one lowercase Latin letter
+                            2) at least one capital Latin letter
+                            3) at least one digit""");
         }
-        Authority authority = new Authority(request.getRole().name());
+        Authority authority = new Authority(request.getRole());
         var user = User.builder()
                 .username(request.getUsername())
                 .email(request.getEmail())
@@ -41,12 +45,39 @@ public class AuthenticationService {
                 .password(passwordEncoder.encode(request.getPassword()))
                 .authorities(List.of(authority))
                 .build();
+
         authority.setUser(user);
-        repository.save(user);
-        var jwtToken = jwtUtil.generateToken(user);
-        return AuthenticationResponse.builder()
-                .token(jwtToken)
-                .build();
+        try {
+            userService.save(user);
+        } catch (Exception e) {
+
+            if (e.toString().contains("email") ||
+                    userService.findUserByEmail(request.getEmail()).isPresent()) {
+                bindingResult.rejectValue("email", "",
+                        """
+                                Email does not meet the criteria:
+                                1) the email field should not be empty
+                                2) the length should be between 5 and 30 characters
+                                3) the email must be unique""");
+            }
+            if (e.toString().contains("username") ||
+                    userService.findUserByUsername(request.getUsername()).isPresent()) {
+                bindingResult.rejectValue("username", "",
+                        """
+                                Username does not meet the criteria:
+                                1) the username field should not be empty
+                                2) the length should be between 1 and 30 characters
+                                3) the username must be unique""");
+            }
+        }
+        if(!bindingResult.hasErrors()) {
+            var jwtToken = jwtUtil.generateToken(user);
+            return AuthenticationResponse.builder()
+                    .token(jwtToken)
+                    .build();
+        } else {
+            return new AuthenticationResponse();
+        }
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
@@ -56,7 +87,7 @@ public class AuthenticationService {
                         request.getPassword()
                 )
         );
-        var user = repository.findByUsername(request.getUsername()).orElseThrow();
+        var user = userService.findUserByUsername(request.getUsername()).orElseThrow();
         var jwtToken = jwtUtil.generateToken(user);
         return AuthenticationResponse.builder()
                 .token(jwtToken)
