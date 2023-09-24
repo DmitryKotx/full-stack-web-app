840232a8aa008e9a723226c5c368a69447dd6961
DimaKotovProgrammist <dskotov003@gmail.com>
2023-09-24 22:51:54 +0300

    deleting an unnecessary class

--- back-end/main/java/ru/kotov/AssignmentSubmissionApp/error/IncorrectPasswordException.java
+++
@@ -1,7 +0,0 @@
-package ru.kotov.AssignmentSubmissionApp.error;
-
-public class IncorrectPasswordException extends Exception{
-    public IncorrectPasswordException(String message) {
-       super(message);
-    }
-}
