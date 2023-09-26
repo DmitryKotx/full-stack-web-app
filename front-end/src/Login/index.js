8955270ed65dd43645a71f33953f02fe056b407c
DimaKotovProgrammist <dskotov003@gmail.com>
2023-09-26 23:46:19 +0300

    added tooltips in the interface if the input data is incorrect

--- front-end/src/Login/index.js
+++ front-end/src/Login/index.js
@@ -1,5 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Button, Col, Container, Row, Form } from "react-bootstrap";
+import {
+    Button,
+    Col,
+    Container,
+    Row,
+    Form,
+    Overlay,
+    Tooltip,
+} from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../UserProvider";
 
@@ -7,6 +15,8 @@ const Login = () => {
     const user = useUser();
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [usernameError, setUsernameError] = useState(null);
+    const [passwordError, setPasswordError] = useState(null);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -29,17 +39,26 @@ const Login = () => {
             body: JSON.stringify(reqBody),
         })
             .then((response) => {
-                if (response.status === 200)
-                    return Promise.all([response.json, response.headers]);
-                else return Promise.reject("Invalid login attempt");
+                if (response.status === 200 || response.status === 401)
+                    return Promise.all([response.json(), response.headers]);
+                else return Promise.reject("Unknow error");
             })
             .then(([body, headers]) => {
-                user.setJwt(headers.get("authorization"));
-                navigate("/dashboard");
+                if (JSON.stringify(body) === "{}") {
+                    user.setJwt(headers.get("authorization"));
+                    navigate("/dashboard");
+                } else {
+                    if (body.username) {
+                        setUsernameError(body.username);
+                        console.log(body.username);
+                    }
+                    if (body.password) {
+                        setPasswordError(body.password);
+                        console.log(body.password);
+                    }
+                }
             })
-            .catch((message) => {
-                alert(message);
-            });
+            .catch((message) => {});
     }
 
     return (
@@ -50,15 +69,35 @@ const Login = () => {
                         <Form.Group className="mb-3" controlId="username">
                             <label className="fs-4">Username</label>
                             <Form.Control
-                                type="email"
-                                id="username"
+                                type="username"
+                                controlId="username"
                                 size="lg"
-                                placeholder="Type in your email"
+                                placeholder="Type in your username"
                                 value={username}
-                                onChange={(event) =>
-                                    setUsername(event.target.value)
-                                }
+                                onChange={(event) => {
+                                    setUsername(event.target.value);
+                                    setUsernameError(null);
+                                }}
+                                isInvalid={usernameError}
                             />
+                            {usernameError ? (
+                                <Overlay
+                                    target={document.getElementById("username")}
+                                    show={usernameError}
+                                    placement="right"
+                                >
+                                    <Tooltip
+                                        id="username-tooltip"
+                                        style={{
+                                            fontSize: "10px",
+                                        }}
+                                    >
+                                        {usernameError}
+                                    </Tooltip>
+                                </Overlay>
+                            ) : (
+                                <></>
+                            )}
                         </Form.Group>
                     </Col>
                 </Row>
@@ -67,15 +106,35 @@ const Login = () => {
                         <Form.Group className="mb-3" controlId="password">
                             <label className="fs-4">Password</label>
                             <Form.Control
+                                controlId="password"
                                 type="password"
-                                id="password"
                                 size="lg"
                                 placeholder="Type in your password"
                                 value={password}
-                                onChange={(event) =>
-                                    setPassword(event.target.value)
-                                }
+                                onChange={(event) => {
+                                    setPassword(event.target.value);
+                                    setPasswordError(null);
+                                }}
+                                isInvalid={passwordError}
                             />
+                            {passwordError ? (
+                                <Overlay
+                                    target={document.getElementById("password")}
+                                    show={passwordError}
+                                    placement="right"
+                                >
+                                    <Tooltip
+                                        id="password-tooltip"
+                                        style={{
+                                            fontSize: "10px",
+                                        }}
+                                    >
+                                        {passwordError}
+                                    </Tooltip>
+                                </Overlay>
+                            ) : (
+                                <></>
+                            )}
                         </Form.Group>
                     </Col>
                 </Row>
