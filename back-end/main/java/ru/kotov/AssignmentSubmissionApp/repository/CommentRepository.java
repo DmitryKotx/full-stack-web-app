bb8363252d07c55e54bdcf38fc7c4f5ce6d7f086
DimaKotovProgrammist <dskotov003@gmail.com>
2023-09-28 23:53:20 +0300

    fixed getting an id

--- back-end/main/java/ru/kotov/AssignmentSubmissionApp/repository/CommentRepository.java
+++ back-end/main/java/ru/kotov/AssignmentSubmissionApp/repository/CommentRepository.java
@@ -10,6 +10,6 @@ import java.util.Set;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query("select c from Comment c where c.assignment.Id = :assignmentId")
+    @Query("select c from Comment c where c.assignment.id = :assignmentId")
     Set<Comment> findByAssignmentId(Long assignmentId);
 }
