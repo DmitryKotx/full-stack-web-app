dfc8d75381ea3cea1ba947293ba084121dff100b
DimaKotovProgrammist <dskotov003@gmail.com>
2023-10-03 23:55:32 +0300

    added TaskController

---
+++ back-end/main/java/ru/kotov/AssignmentSubmissionApp/controller/TaskController.java
@@ -0,0 +1,19 @@
+package ru.kotov.AssignmentSubmissionApp.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.kotov.AssignmentSubmissionApp.service.TaskService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/tasks")
+public class TaskController {
+    private final TaskService taskService;
+    @GetMapping()
+    public ResponseEntity<?> getTasks() {
+        return ResponseEntity.ok(taskService.getTasks());
+    }
+}
