a4c6472462fc8ca28c5755c657e7a40d88998a36
DimaKotovProgrammist <dskotov003@gmail.com>
2023-10-06 23:08:12 +0300

    minor change

--- back-end/main/java/ru/kotov/AssignmentSubmissionApp/controller/TaskController.java
+++ back-end/main/java/ru/kotov/AssignmentSubmissionApp/controller/TaskController.java
@@ -2,9 +2,11 @@ package ru.kotov.AssignmentSubmissionApp.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.kotov.AssignmentSubmissionApp.model.User;
 import ru.kotov.AssignmentSubmissionApp.service.TaskService;
 
 @RestController
@@ -13,7 +15,7 @@ import ru.kotov.AssignmentSubmissionApp.service.TaskService;
 public class TaskController {
     private final TaskService taskService;
     @GetMapping()
-    public ResponseEntity<?> getTasks() {
+    public ResponseEntity<?> getTasks(@AuthenticationPrincipal User user) {
         return ResponseEntity.ok(taskService.getTasks());
     }
 }
