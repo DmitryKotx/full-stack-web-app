f2cac1ca2005aa4597fc8a1513de0d5e0488f3e3
DimaKotovProgrammist <dskotov003@gmail.com>
2023-10-07 17:48:43 +0300

    Added methods to the TaskController class

--- back-end/main/java/ru/kotov/AssignmentSubmissionApp/controller/TaskController.java
+++ back-end/main/java/ru/kotov/AssignmentSubmissionApp/controller/TaskController.java
@@ -3,9 +3,8 @@ package ru.kotov.AssignmentSubmissionApp.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.kotov.AssignmentSubmissionApp.model.Task;
 import ru.kotov.AssignmentSubmissionApp.model.User;
 import ru.kotov.AssignmentSubmissionApp.service.TaskService;
 
@@ -17,5 +16,20 @@ public class TaskController {
     @GetMapping()
     public ResponseEntity<?> getTasks(@AuthenticationPrincipal User user) {
         return ResponseEntity.ok(taskService.getTasks());
+    }
+    @PostMapping("")
+    public ResponseEntity<?> addTask(@AuthenticationPrincipal User user) {
+        Task task = taskService.save(new Task());
+        return ResponseEntity.ok(task);
+    }
+    @PutMapping("{id}")
+    public ResponseEntity<?> updateTask(@PathVariable Long id, @RequestBody Task task, @AuthenticationPrincipal User user) {
+        Task updateTask = taskService.save(task);
+        return ResponseEntity.ok(updateTask);
+    }
+    @GetMapping("{id}")
+    public ResponseEntity<?> getTask(@PathVariable Long id, @AuthenticationPrincipal User user) {
+        Task updateTask = taskService.getTask(id).orElse(null);
+        return ResponseEntity.ok(updateTask);
     }
 }
