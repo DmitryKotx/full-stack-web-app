11002577671865885242a0005cf4f2a3eb41b344
DimaKotovProgrammist <dskotov003@gmail.com>
2023-10-08 22:55:44 +0300

    changed the way the task list is presented, added the use of style for span

--- front-end/src/AssignmentTasks/index.js
+++ front-end/src/AssignmentTasks/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useUser } from "../UserProvider";
 import ajax from "../Services/fetchService";
+import { Container, ListGroup } from "react-bootstrap";
 
 const AssignmentTasks = () => {
     const user = useUser();
@@ -12,16 +13,20 @@ const AssignmentTasks = () => {
         });
     }, [user.jwt]);
     return (
-        <div className="container">
+        <Container className="mt-4 mb-4">
             <h1>Tasks</h1>
-            <ul className="list-group">
-                {tasks.map((task, index) => (
-                    <li key={index} className="list-group-item">
-                        Task {task.id}: {task.text}
-                    </li>
+            <ListGroup>
+                {tasks.map((task) => (
+                    <ListGroup.Item key={task.id}>
+                        <div className="d-flex justify-content-between align-items-center">
+                            <span className="task-text">
+                                Task {task.id}: {task.text}
+                            </span>
+                        </div>
+                    </ListGroup.Item>
                 ))}
-            </ul>
-        </div>
+            </ListGroup>
+        </Container>
     );
 };
 
