d992ffa0b28fa7fa33719534d34c9ff5658c8aa8
DimaKotovProgrammist <dskotov003@gmail.com>
2023-10-11 23:59:35 +0300

    added creating and sending json if the task has an empty text field

--- back-end/main/java/ru/kotov/AssignmentSubmissionApp/controller/TaskController.java
+++ back-end/main/java/ru/kotov/AssignmentSubmissionApp/controller/TaskController.java
@@ -1,12 +1,19 @@
 package ru.kotov.AssignmentSubmissionApp.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import ru.kotov.AssignmentSubmissionApp.model.Task;
 import ru.kotov.AssignmentSubmissionApp.model.User;
 import ru.kotov.AssignmentSubmissionApp.service.TaskService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,9 +30,20 @@ public class TaskController {
         return ResponseEntity.ok(task);
     }
     @PutMapping("{id}")
-    public ResponseEntity<?> updateTask(@PathVariable Long id, @RequestBody Task task, @AuthenticationPrincipal User user) {
-        Task updateTask = taskService.save(task);
-        return ResponseEntity.ok(updateTask);
+    public ResponseEntity<?> updateTask(@PathVariable Long id, @RequestBody Task task, @AuthenticationPrincipal User user, BindingResult bindingResult) throws JsonProcessingException {
+
+        Task updateTask = taskService.save(task, bindingResult);
+        if(bindingResult.hasErrors()) {
+            Map<String, String> validationErrors = new HashMap<>();
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                validationErrors.put(error.getField(), error.getDefaultMessage());
+            }
+            ObjectMapper objectMapper = new ObjectMapper();
+            String json = objectMapper.writeValueAsString(validationErrors);
+            return ResponseEntity.ok(json);
+        } else {
+            return ResponseEntity.ok(updateTask);
+        }
     }
     @GetMapping("{id}")
     public ResponseEntity<?> getTask(@PathVariable Long id, @AuthenticationPrincipal User user) {
