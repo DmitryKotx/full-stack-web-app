7c226b4b5daed1bec4da810b8087098eebdd4b2f
DimaKotovProgrammist <dskotov003@gmail.com>
2023-10-12 23:57:43 +0300

    added a return with a warning that the text field is empty

--- back-end/main/java/ru/kotov/AssignmentSubmissionApp/controller/CommentController.java
+++ back-end/main/java/ru/kotov/AssignmentSubmissionApp/controller/CommentController.java
@@ -1,13 +1,16 @@
 package ru.kotov.AssignmentSubmissionApp.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.kotov.AssignmentSubmissionApp.dto.CommentDTO;
 import ru.kotov.AssignmentSubmissionApp.model.Comment;
 import ru.kotov.AssignmentSubmissionApp.model.User;
 import ru.kotov.AssignmentSubmissionApp.service.CommentService;
+import ru.kotov.AssignmentSubmissionApp.util.JsonUtil;
 
 import java.util.Set;
 
@@ -18,15 +21,25 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("")
-    public ResponseEntity<Comment> createComment(@RequestBody CommentDTO commentDTO, @AuthenticationPrincipal User user) {
-        Comment comment = commentService.save(commentDTO, user);
-        return ResponseEntity.ok(comment);
+    public ResponseEntity<?> createComment(@RequestBody CommentDTO commentDTO, @AuthenticationPrincipal User user,
+                                                 BindingResult bindingResult) throws JsonProcessingException {
+        Comment comment = commentService.save(commentDTO, user, bindingResult);
+        if(bindingResult.hasErrors()) {
+            return ResponseEntity.ok(JsonUtil.getJson(bindingResult));
+        } else {
+            return ResponseEntity.ok(comment);
+        }
     }
 
     @PutMapping("{commentId}")
-    public ResponseEntity<Comment> updateComment(@RequestBody CommentDTO commentDTO, @AuthenticationPrincipal User user, @PathVariable String commentId) {
-        Comment comment = commentService.save(commentDTO, user);
-        return ResponseEntity.ok(comment);
+    public ResponseEntity<?> updateComment(@RequestBody CommentDTO commentDTO, @AuthenticationPrincipal User user,
+                                                 @PathVariable String commentId, BindingResult bindingResult) throws JsonProcessingException {
+        Comment comment = commentService.save(commentDTO, user, bindingResult);
+        if(bindingResult.hasErrors()) {
+            return ResponseEntity.ok(JsonUtil.getJson(bindingResult));
+        } else {
+            return ResponseEntity.ok(comment);
+        }
     }
     @DeleteMapping("{commentId}")
     public ResponseEntity<Comment> deleteComment(@PathVariable Long commentId, @AuthenticationPrincipal User user ) {
