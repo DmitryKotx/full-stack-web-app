c8a1c746ffa4a5aa11cb1e2cca91c1ce0962db1b
DimaKotovProgrammist <dskotov003@gmail.com>
2023-10-13 00:06:38 +0300

    the structure of AssignmentStatusEnum has been changed

--- back-end/main/java/ru/kotov/AssignmentSubmissionApp/enums/AssignmentStatusEnum.java
+++ back-end/main/java/ru/kotov/AssignmentSubmissionApp/enums/AssignmentStatusEnum.java
@@ -4,27 +4,24 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum AssignmentStatusEnum {
-    PENDING_SUBMISSION("Pending Submission", 1),
-    SUBMITTED("Submitted", 2),
-    IN_REVIEW("In Review", 3),
-    NEEDS_UPDATE("Needs Update", 4),
-    COMPLETED("Completed", 5),
-    RESUBMITTED("Resubmitted", 6);
+    PENDING_SUBMISSION("Pending Submission"),
+    SUBMITTED("Submitted"),
+    IN_REVIEW("In Review"),
+    NEEDS_UPDATE("Needs Update"),
+    COMPLETED("Completed"),
+    RESUBMITTED("Resubmitted");
 
 
     private final String status;
-    private final Integer step;
 
-    AssignmentStatusEnum(String status, Integer step) {
+
+    AssignmentStatusEnum(String status) {
         this.status = status;
-        this.step = step;
     }
 
     public String getStatus() {
         return status;
     }
 
-    public Integer getStep() {
-        return step;
-    }
+
 }
